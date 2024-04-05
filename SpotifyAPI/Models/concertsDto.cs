0dc2773d3996ddfb1ef24c2d479f27bd7e08985c
MeGa <MeGa@DESKTOP-FOV766R>
2024-04-05 12:37:24 +0300

    Add project files.

---
+++ SpotifyAPI/Models/concertsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SpotifyAPI.Models
+{
+    public class concertsDto
+    {
+        public string eventTitle { set; get; }
+        public string eventDate { set; get; }
+        public string venue { set; get; }
+        public int artistsCount { set; get; }
+        public int concertsCount { set; get; }
+    }
+}
